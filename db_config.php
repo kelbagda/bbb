88e70aeaeabea241c6e33958e4847e18da70faed
kelbagda <kelbagda@uwo.ca>
2016-03-12 11:58:24 -0500

    Added files via upload

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "BBBkelbagda"); // db user
+define('DB_PASSWORD', "BBBpassword!1"); // db password (mention your db password here)
+define('DB_DATABASE', "BBBkelbagda"); // database name
+define('DB_SERVER', "BBBkelbagda.db.9367618.hostedresource.com"); // db server
+?>
